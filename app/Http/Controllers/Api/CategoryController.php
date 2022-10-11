802ef91d951384dee6807732d0d547364e7e5870
Leonardo <contato@leowebdesigner.com.br>
2022-10-11 16:29:56 -0300

    Create function Store

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -8,10 +8,25 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function index(Category $category, Request $request)
+   
+    private $category;
+
+    public function __construct(Category $category)
     {
-        $categories = $category->getResults($request->name);
+       $this->category = $category;
+    }
+
+    public function index(Request $request)
+    {
+        $categories = $this->category->getResults($request->name);
 
         return response()->json($categories,200);
+    }
+
+    public function store(Request $request)
+    {
+        $category= $this->category->create($request->all());
+        
+        return response()->json($category,201);
     }
 }
