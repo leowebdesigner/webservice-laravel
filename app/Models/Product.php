8fb71a4a6e2c938424915341fec5fd43f2c6a917
Leonardo <contato@leowebdesigner.com.br>
2022-10-14 16:14:40 -0300

    factory products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
+   use HasFactory;
+   protected $fillable = ['name','description','image'];
 }
--- database/migrations/2022_10_14_151939_create_products_table.php
+++ database/migrations/2022_10_14_151939_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name',100)->unique();
-            $table->string('description')->nullable();
+            $table->string('description',200)->nullable();
             $table->string('image')->nullable();
             $table->timestamps();
         });
